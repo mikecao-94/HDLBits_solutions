526b9beab937b45b72e9fb7707b81df8130d7096
Mike <mikecao94@gmail.com>
2025-06-08 22:00:00 -0500

    update

--- Verilog_Language/Module_Hierarchy/Module_cseladd.v
+++ Verilog_Language/Module_Hierarchy/Module_cseladd.v
@@ -3,13 +3,32 @@ module top_module(
     input [31:0] b,
     output [31:0] sum
 );
-    wire [15:0] sum_hi0, sum_hi1, sum_hi, sum_lo;
-    wire cout;
-
-    add16 add_hi1   ();
-    add16 add_hi2   ();
-    add16 add_lo    ();
+    wire [15:0] sum_lo, sum_hi;
+    wire [15:0] sum_hi_0, sum_hi_1;
+ 
+    wire sel;
+    add16 adder_1a (.a(a[31:16]),
+                  .b(b[31:16]),
+                  .cin(1'b0),
+                  .sum(sum_hi_0),
+                  .cout()
+                 );
+     add16 adder_1b (.a(a[31:16]),
+                  .b(b[31:16]),
+                  .cin(1'b1),
+                  .sum(sum_hi_1),
+                  .cout()
+                 );
+    add16 adder2 (.a(a[15:0]),
+                  .b(b[15:0]),
+                  .cin(1'b0),
+                  .sum(sum_lo),
+                  .cout(sel)
+                 );
     
-    assign sum = (cout) ? sum_hi : sum_lo;
+    //mux to select between 2 sum_hi
+    assign sum_hi = (sel) ? sum_hi_1 : sum_hi_0;
+    
+    assign sum = {sum_hi,sum_lo};
 
 endmodule
