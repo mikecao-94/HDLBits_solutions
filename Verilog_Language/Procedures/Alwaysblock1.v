b72ee8dcee5d3a8ae712580dcf5b5709d08f7127
Mike <mikecao94@gmail.com>
2025-06-08 22:37:35 -0500

    update

---
+++ Verilog_Language/Procedures/Alwaysblock1.v
@@ -0,0 +1,14 @@
+// synthesis verilog_input_version verilog_2001
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+
+    always@(*)
+    out_alwaysblock = a & b;
+
+    assign out_assign = a & b;
+    
+endmodule
